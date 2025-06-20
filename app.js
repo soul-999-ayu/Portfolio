c132b01e4b7b2db78fa52af5ce010f0d1a24f921
Ayu Kashyap <91123494+soul-999-ayu@users.noreply.github.com>
2025-06-20 14:04:06 +0530

    code

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const popup = document.getElementById('colorPopup');
+const overlay = document.getElementById('overlay');
+const colorPicker = document.getElementById('colorPicker');
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -8,6 +12,37 @@
         })
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+        // document.body.classList.toggle("light-mode");
+        console.log("clicked");
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
     })
+    overlay.addEventListener('click', () => {
+        popup.style.display = 'none';
+        overlay.style.display = 'none';
+    });
 })();
+
+function setColor(){
+    let value = localStorage.getItem('themeColor');
+    if(value==null){
+        localStorage.setItem('themeColor', '#ff4c4c');
+        colorPicker.value = '#ff4c4c';
+    }
+    else{
+        document.documentElement.style.setProperty('--color-secondary', value);
+        colorPicker.value = value;
+    }
+}
+
+function closeP(){
+    popup.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function saveP(){
+    closeP()
+    const selectedColor = colorPicker.value;
+    localStorage.setItem('themeColor', selectedColor);
+    document.documentElement.style.setProperty('--color-secondary', selectedColor);
+}
